1d085eb26fdfce069951d96e145ab7b26343ffab
unknwon1122 <nizargans954@gmail.com>
2026-01-22 19:15:05 +0700

    Update pending_request.js

--- pending_request.js
+++ pending_request.js
@@ -1,11 +1,1 @@
-[
-  {
-    "id": "1",
-    "username": "Roblox - Roblox",
-    "ip": "182.10.129.4",
-    "hwid": "5816B43E00B5CB78260122000020B9",
-    "status": "pending",
-    "createdAt": "2026-01-22T12:11:34.488Z",
-    "verifiedBy": "Not Verified"
-  }
-]
+
