0ca817022df561ed3ac4baba9c81c5820cddc37d
unknwon1122 <nizargans954@gmail.com>
2026-01-22 19:11:34 +0700

    [SAWIT TEAM] Add pending request for Roblox - Roblox (1)

--- pending_request.js
+++ pending_request.js
@@ -1,1 +1,11 @@
-
+[
+  {
+    "id": "1",
+    "username": "Roblox - Roblox",
+    "ip": "182.10.129.4",
+    "hwid": "5816B43E00B5CB78260122000020B9",
+    "status": "pending",
+    "createdAt": "2026-01-22T12:11:34.488Z",
+    "verifiedBy": "Not Verified"
+  }
+]
